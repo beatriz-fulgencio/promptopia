be9dda990ad80c1eb18955e064d251bb0776ab51
Beatriz Fulgêncio <beatrizfulgencio03@gmail.com>
2023-06-05 18:01:47 -0300

    5/6 -> until nav bar (1:02:00)

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+import '@styles/globals.css';
+//import { Children } from 'react';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+
+export const metadata = {
+    title:"Promptopia",
+    description:"Discover and share AI Propts"
+}
+
+
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+    <body>
+        <div className='main'>
+            <div className='gradient' />
+        </div>
+
+        <main className='app'>
+            <Nav/>
+            {children}
+        </main>
+
+    </body>
+    </html>
+  )
+}
+
+export default RootLayout
